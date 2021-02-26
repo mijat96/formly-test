a9e2c94dbf5c9ee17d9ad74e083c1849490b42c5
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2021-02-26 09:07:52 +0100

    feat: Added Sections 1 and 2 of Design document

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'formly-test';
+  form1 = new FormGroup({});
+  model1 = {};
+  fields1: FormlyFieldConfig[] = [
+    {
+      key: 'transferOfFunds',
+      type: 'input',
+      templateOptions: {
+        label: 'Request Subject',
+        placeholder: 'Transfer of funds',
+        required: true,
+      },
+    },
+  ];
+
+  form2 = new FormGroup({});
+  model2 = {};
+  fields2: FormlyFieldConfig[] = [
+    {
+      fieldGroupClassName: 'row',
+      fieldGroup: [
+        {
+          className: 'col-4',
+          key: 'submissionType',
+          type: 'input',
+          templateOptions: {
+            label: 'Submission Type',
+            disabled: true,
+          },
+        },
+        {
+          className: 'col-4',
+          key: 'userDirectorate',
+          type: 'input',
+          templateOptions: {
+            label: 'User Directorate',
+            disabled: true,
+          },
+        },
+        {
+          className: 'col-4',
+          key: 'client',
+          type: 'input',
+          templateOptions: {
+            label: 'Client',
+          },
+        },
+      ],
+    },
+    {
+      fieldGroupClassName: 'row',
+      fieldGroup: [
+        {
+          className: 'col-4',
+          key: 'proccurmentEngeneer',
+          type: 'input',
+          templateOptions: {
+            label: 'Proccurment Engeneer',
+            disabled: true,
+          },
+        },
+        {
+          className: 'col-4',
+          key: 'fdqa',
+          type: 'input',
+          templateOptions: {
+            label: 'FDQA',
+            disabled: true,
+          },
+        },
+        {
+          className: 'col-4',
+          key: 'tenderNumber',
+          type: 'input',
+          templateOptions: {
+            label: 'Tender Number',
+            type: 'number',
+          },
+        },
+      ],
+    },
+  ];
+
+  onSubmit() {
+    console.log({ form: this.form1.value });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { FormlyModule } from '@ngx-formly/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    FormlyModule.forRoot(),
+    FormlyBootstrapModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
